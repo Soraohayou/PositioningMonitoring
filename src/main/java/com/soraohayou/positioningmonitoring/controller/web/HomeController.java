1c230f9517cc92d46e8cf4fbbef23730e76e3910
坚石-郭子靖 <kaibu@keji.com>
2024-04-19 19:40:17 +0800

    2024-4-15，建立web框架

--- src/main/java/com/soraohayou/positioningmonitoring/controller/web/HomeController.java
+++ src/main/java/com/soraohayou/positioningmonitoring/controller/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.soraohayou.positioningmonitoring.controller;
+package com.soraohayou.positioningmonitoring.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/soraohayou/positioningmonitoring/controller/web/UserController.java
+++ src/main/java/com/soraohayou/positioningmonitoring/controller/web/UserController.java
@@ -1,18 +1,20 @@
-package com.soraohayou.positioningmonitoring.controller;
+package com.soraohayou.positioningmonitoring.controller.web;
 
 import com.soraohayou.positioningmonitoring.entity.User;
 import com.soraohayou.positioningmonitoring.exception.CustomException;
 import com.soraohayou.positioningmonitoring.service.UserService;
 import jakarta.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -20,32 +22,41 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/getAllUser")
+    @ResponseBody
     public List<User> getAllUser() {
         return userService.getUserListByCondition(new HashMap<>());
     }
 
     @PostMapping("/getAllUserByCondition")
+    @ResponseBody
     public List<User> getUserByCondition(String realName, String teamId) {
-        Map<String,Object> params = new HashMap<>();
-        params.put("realName",realName);
-        params.put("teamId",teamId);
+        Map<String, Object> params = new HashMap<>();
+        params.put("realName", realName);
+        params.put("teamId", teamId);
         return userService.getUserListByCondition(params);
     }
 
     @PostMapping("/login")
+    @ResponseBody
     public User login(String username, String password) {
         Map<String, Object> params = new HashMap<>();
         params.put("username", username);
         params.put("password", password);
         User user = userService.getUserByCondition(params);
-        if (user == null) {
+        if (user != null) {
             return user;
         } else {
             throw new CustomException("用户名密码错误");
         }
     }
 
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "/login";
+    }
+
     @PostMapping("/register")
+    @ResponseBody
     public void register(User userModel) {
         Map<String, Object> params = new HashMap<>();
         params.put("username", userModel.getUserName());
